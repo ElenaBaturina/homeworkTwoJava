64daa3ef87f58b5ca8ebd28549fc176830dde6fe
[Elena] <[liabaturina@yandex.ru]>
2022-05-01 11:07:51 +0300

    Initial commit.

--- src/homeworkOne.java
+++ src/homeworkOne.java
@@ -1,7 +1,15 @@
 public class homeworkOne {
 
+    public static void main(String[] args) {
+        double gallons; // в этой переменной хранится значение ,
+        // вьражающее объем жидкости в галлонах
+        double liters; // в этой переменной хранится значение ,
+        // вьражающе е объем жидкости в литрах
+        gallons = 10; // начать ное значение соответст вует 10 галлонам
+        liters = gallons * 3.7854; // перевод в литры
+        System.out.println(gallons + " галлонам соответствует " +
+                liters + " литра ");
 
 
-
-
+    }
 }
