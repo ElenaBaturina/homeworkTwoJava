5b732f8d621475bcfbdacea40b8b9619ba2cbec0
[Elena] <[liabaturina@yandex.ru]>
2022-05-04 17:31:19 +0300

    Initial commit.

--- src/homeworkOne.java
+++ src/homeworkOne.java
@@ -11,26 +11,21 @@ public class homeworkOne {
                 liters + " литра ");
 
 
-    }
-    private int id;
-    private String name;
-    private String position;
-    private int age;
-    public homeworkOne(int id, String name, String position, int age) {
-        this.id = id;
-        this.name = name;
-        this.position = position;
-        this.age = age;
-    }
-    public void info() {
-        System.out.println("id: " + id + "; Имя пользователя: " + name+ "Должность: " + position + "; Возраст: " + age);
-    }
-    public void changePosition(String position) {
-        this.position = position;
-        System.out.println("Пользователь " + name + " получил новую должность: "
-                + position);
-    }
+        int x;
+        for ( x = 0; x < 6; x++ ) {
+            if ( x == 1)
+                System.out.println(" x равно 1");
+            else if ( x == 2 )
+                System.out.println(" x равно 2");
+            else if ( x == 3 )
+                System.out.println(" x равно 3 ");
+            else if ( x == 4 )
+                System.out.println("x равно 4");
+            else
+                System.out.println(" Знaчeниe х н аходится вне диапазона 1 - 4 ");
+        }
 
+        }
 
 
 }
