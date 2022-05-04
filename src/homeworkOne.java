a292d566ab971701507dc9722fa2b046b9b0713d
[Elena] <[liabaturina@yandex.ru]>
2022-05-04 16:54:01 +0300

    Initial commit.

--- src/homeworkOne.java
+++ src/homeworkOne.java
@@ -12,4 +12,25 @@ public class homeworkOne {
 
 
     }
+    private int id;
+    private String name;
+    private String position;
+    private int age;
+    public homeworkOne(int id, String name, String position, int age) {
+        this.id = id;
+        this.name = name;
+        this.position = position;
+        this.age = age;
+    }
+    public void info() {
+        System.out.println("id: " + id + "; Имя пользователя: " + name+ "Должность: " + position + "; Возраст: " + age);
+    }
+    public void changePosition(String position) {
+        this.position = position;
+        System.out.println("Пользователь " + name + " получил новую должность: "
+                + position);
+    }
+
+
+
 }
